fcfaf3d6b1eeff5f9fc59d2f47b7102ffce7341e
krseo <keseo71@ibizsoftware.net>
2024-12-27 17:23:03 +0900

    volatile 메모리 가시성

---
+++ src/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,36 @@
+package thread.volatile1;
+
+import util.MyLogger;
+import util.ThreadUtils;
+
+import static util.MyLogger.*;
+import static util.ThreadUtils.*;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+//        boolean runFlag = true; // 캐시 메모리에 접근
+        volatile boolean runFlag = true; // 메인 메모리에 직접 접근
+        @Override
+        public void run() {
+            log("task 시작");
+            while (runFlag) {
+                // runFlag가 false로 변하면 탈출
+            }
+            log("task 종료");
+        }
+    }
+}

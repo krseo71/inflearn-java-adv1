17d7bb6f8d160f0b4f8b433bdf96da3053e7b816
krseo <keseo71@ibizsoftware.net>
2024-12-30 10:12:02 +0900

    동기화 bankAccount 예시

---
+++ src/thread/sync/BankMain.java
@@ -0,0 +1,30 @@
+package thread.sync;
+
+import util.MyLogger;
+import util.ThreadUtils;
+
+import static util.MyLogger.*;
+import static util.ThreadUtils.*;
+
+public class BankMain {
+    public static void main(String[] args) throws InterruptedException {
+        BankAccountV1 account = new BankAccountV1(1000);
+        Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
+        t1.start();
+        t2.start();
+
+        sleep(500);
+        log("t1 state: " + t1.getState());
+        log("t2 state: " + t2.getState());
+
+        t1.join();
+        t2.join();
+        log("최종 잔액: " + account.getBalance());
+    }
+}
+/**
+ * 임계 영역(critical section)
+ * 여러 스레드가 동시에 접근하면 데이터 불일치나 예상치 못한 동작이 발생할 수 있는 위험하고 또 중요한 코드 부분
+ * 여러 스레드가 동시에 접근해서는 안되는 공유자원을 접근하거나 수정하는 부분을 의미
+ */

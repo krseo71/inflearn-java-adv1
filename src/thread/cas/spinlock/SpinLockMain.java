32a886cc429e7e6220d4260379d144a4babd0649
krseo <keseo71@ibizsoftware.net>
2024-12-31 17:06:16 +0900

    AtomicXXX 예제

---
+++ src/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,33 @@
+package thread.cas.spinlock;
+
+import util.MyLogger;
+import util.ThreadUtils;
+
+import static util.MyLogger.*;
+import static util.ThreadUtils.*;
+
+public class SpinLockMain {
+    public static void main(String[] args) {
+        SpinLockBad spinLock = new SpinLockBad();
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                spinLock.lock();
+                try {
+                    // critical section
+                    log("비즈니스 로직 실행");
+                    //sleep(1); // 오래 걸리는 로직에서 스핀 락 사용 X
+                } finally {
+                    spinLock.unlock();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
